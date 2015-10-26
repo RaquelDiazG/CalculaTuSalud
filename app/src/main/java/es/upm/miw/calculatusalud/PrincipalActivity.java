5db76e99f1619a5338ca07a697b88af30af955f5
Raquel Díaz González <raquel.diazgonz@alumnos.upm.es>
2015-10-26 20:10:09 +0100

    Modificada PrincipalActivity

--- app/src/main/java/es/upm/miw/calculatusalud/PrincipalActivity.java
+++ app/src/main/java/es/upm/miw/calculatusalud/PrincipalActivity.java
@@ -2,6 +2,7 @@ package es.upm.miw.calculatusalud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void mostrarIMC(View view) {
+    }
+
+    public void mostrarPeso(View view) {
+    }
+
+    public void mostrarMetabolismo(View view) {
+    }
+
+    public void mostrarCalorias(View view) {
     }
 }
